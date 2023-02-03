b9f977694d429379c4e6042f52f75ca6f7532d56
BringMeTheIoannis <ivankuzmenkov@icloud.com>
2023-02-03 13:43:38 +0300

    Initial commit

---
+++ ClubsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClubsApp
+//
+//  Created by Ivan Kuzmenkov on 3.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
