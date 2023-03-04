7685595b1c55df6ac13adcf393c5dd192d39b303
BringMeTheIoannis <86875034+BringMeTheIoannis@users.noreply.github.com>
2023-03-04 22:09:05 +0300

    done pop up vc created

--- ClubsApp/Controllers/CreateEventViewController/DonePopUpViewController.swift
+++ ClubsApp/Controllers/CreateEventViewController/DonePopUpViewController.swift
@@ -9,11 +9,66 @@ import UIKit
 
 class DonePopUpViewController: UIViewController {
     
+    var doneView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 13
+        return view
+    }()
     
+    var stackView: UIStackView = {
+        var stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 14
+        return stackView
+    }()
+    
+    var imageView: UIImageView = {
+        var imageView = UIImageView()
+        var image = UIImage(named: "doneImage")
+        imageView.image = image
+        return imageView
+    }()
+    
+    var label: UILabel = {
+        var label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Успешно создано!"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .black.withAlphaComponent(0.3)
+        addSubviews()
+        doLayout()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(doneView)
+        doneView.addSubview(stackView)
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+    }
+    
+    private func doLayout() {
+        doneView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.centerX.centerY.equalToSuperview()
+            make.height.equalTo(300)
+        }
         
+        stackView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.height.equalTo(87)
+            make.width.equalTo(89)
+        }
     }
 }
